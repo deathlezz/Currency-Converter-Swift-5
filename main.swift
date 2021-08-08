2efbeaad518272a933496da56623c0d8d7f6af5d
deathlezz <78265796+deathlezz@users.noreply.github.com>
2021-08-08 18:21:28 +0200

    Create main.swift

---
+++ main.swift
@@ -0,0 +1,98 @@
+//
+// Currency Converter
+//
+// Created by deathlezz on 8/08/2021.
+//
+// https://api.frankfurter.app/latest?from=USD
+
+import Foundation
+
+// available currencies array
+let currency = ["USD", "EUR", "JPY", "GBP"]
+
+func convert(base: String, to: String, amount: Float) {
+
+    // specify how the json file looks like
+    struct json: Codable {
+        let base: String
+        let rates: [String: Float]
+    }
+
+    // create url
+    if let url = URL(string: "https://api.frankfurter.app/latest?from=\(base)") {
+
+        do {
+            // make http (get) call
+            let contents = try String(contentsOf: url)
+
+            // specify decoding format
+            let data = contents.data(using: .utf8)
+
+            // decode json data
+            let ratesData = try JSONDecoder().decode(json.self, from: data!)
+
+            if base != to {
+                // multiply amount with rates
+                let multiply = amount * ratesData.rates["\(to)"]!
+            
+                // output
+                print("\(amount) \(base) = \(multiply) \(to)")
+            }
+        } catch {
+            print("* Check connection *")
+        }
+    } else {
+        print("* Invalid URL *")
+    }
+}
+
+print("* Welcome to Currency Converter *")
+
+func enterBase() {
+    print()
+    print("Enter base currency: [USD, EUR, JPY, GBP]")
+
+    let base = readLine()
+        
+    if base == "USD" || base == "EUR" || base == "JPY" || base == "GBP" {
+
+        func enterAmount() {
+            print()
+            print("Enter amount:")
+
+            if let number = Float(readLine()!) {
+                
+                if number > 0 {
+                    print()
+                    
+                    // call the function
+                    for name in currency {
+                        convert(base: base!, to: name, amount: number)
+                    }
+                    return enterBase()
+                    
+                } else {
+                    print()
+                    print("* Enter value bigger than 0 *")
+                    return enterAmount()
+                }
+                    
+            } else {
+                print()
+                print("* Enter numbers only *")
+                return enterAmount()
+            }
+        }
+        enterAmount()
+        
+    } else {
+        print()
+        print("* Enter USD, EUR, JPY or GBP *")
+        return enterBase()
+    }
+}
+enterBase()
+
+// 5.0 GBP = 6.9585 USD
+// 5.0 GBP = 5.8935 EUR
+// 5.0 GBP = 764.05 JPY
